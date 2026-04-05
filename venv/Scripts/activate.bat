91b3ceb4ddf6152a34523e038bad865e5211d9a4
SapirBoo <sapirbuz5@gmail.com>
2026-04-05 15:14:37 +0300

    Init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Administrator.DESKTOP-QK5R1H1\Documents\Projects\RestApiProject\venv
+set VIRTUAL_ENV=C:\Users\sapir\Documents\RestApiProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
