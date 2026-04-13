967857eadb8ba22c1c726a3c1715b114e8a5170e
SapirBoo <sapirbuz5@gmail.com>
2026-04-13 12:35:55 +0300

    Add redis queue to manage emails send

--- redis_client.py
+++ redis_client.py
@@ -2,5 +2,4 @@ import os
 from redis import Redis
 
 def get_redis():
-    print("REDIS_URL =", os.getenv("REDIS_URL"))                                                                                                 
     return Redis.from_url(os.getenv("REDIS_URL"))
